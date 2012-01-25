cb1d50ddd635abdbfce8ec974039e2b4ba5537dc
Katsuya Noguchi <katsuya@twitter.com>
2012-01-25 10:47:02 -0800

    Version 0.2.1

--- src/myna.js
+++ src/myna.js
@@ -9,7 +9,7 @@
     } else {
       Myna = root.Myna = {};
     }
-    Myna.VERSION = '0.2.0';
+    Myna.VERSION = '0.2.1';
     Myna.noConflict = function() {
       root.Myna = previousMyna;
       return this;
